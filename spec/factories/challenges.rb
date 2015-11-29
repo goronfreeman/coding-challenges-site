d89ec8eada8b72af2c458136076a3cac66c27d66
Hunter Braun <hunter.braun@gmail.com>
2015-11-29 02:59:28 -0600

    remove FactoryGirl

--- spec/factories/challenges.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :challenge do
-    name { Faker::App.name }
-    short_description { Faker::Hipster.sentence(3) }
-    long_description { Faker::Hipster.sentence(8) }
-  end
-end
