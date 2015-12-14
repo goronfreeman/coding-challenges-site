4b8fb636daf7b4067bdb7a56673887bf7d1c982c
Hunter Braun <hunter.braun@gmail.com>
2015-12-14 16:48:57 -0600

    small refactor to challenges controller

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,20 +1,12 @@
 class ChallengesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_challenge, only: [:show, :edit, :update, :destroy]
 
   def index
     @challenges = Challenge.all
-    respond_to do |format|
-      format.html
-      format.json { render json: @challenges }
-    end
   end
 
   def show
-    @challenge = Challenge.find(params[:id])
-    respond_to do |format|
-      format.html
-      format.json { render json: @challenge }
-    end
   end
 
   def new
@@ -32,12 +24,10 @@ class ChallengesController < ApplicationController
   end
 
   def edit
-    @challenge = Challenge.find(params[:id])
+    redirect_to root_path if @challenge.user_id != current_user.id
   end
 
   def update
-    @challenge = Challenge.find(params[:id])
-
     if @challenge.update(challenge_params)
       redirect_to root_path
     else
@@ -46,19 +36,17 @@ class ChallengesController < ApplicationController
   end
 
   def destroy
-    @challenge = Challenge.find(params[:id])
-
-    if @challenge.user_id == current_user.id
-      @challenge.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
+    @challenge.destroy if @challenge.user_id == current_user.id
+    redirect_to root_path
   end
 
   private
 
   def challenge_params
     params.require(:challenge).permit(:name, :short_description, :long_description, :user_id, :difficulty)
+  end
+
+  def find_challenge
+    @challenge = Challenge.find(params[:id])
   end
 end
