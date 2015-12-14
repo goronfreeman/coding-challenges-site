cac5ce18f969adcf8aa60dc0bc00f87268f9d1f0
Hunter Braun <hunter.braun@gmail.com>
2015-12-14 16:49:57 -0600

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 user_one = User.create!(email: 'test@test.com', password: 'password', password_confirmation: 'password')
 
-challenge_one = user_one.challenges.create!(name: 'Lorem Ipsum', short_description: 'dolor sit amet.', long_description: 'consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+challenge_one = user_one.challenges.create!(name: 'Lorem Ipsum', short_description: 'Dolor sit amet.', long_description: 'Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
 
 tags = ['Accessibility', 'API', 'Auth & Service',
         'CMS', 'CSS', 'Database', 'Environments & Server', 'Git', 'JavaScript',
@@ -9,3 +9,5 @@ tags = ['Accessibility', 'API', 'Auth & Service',
 tags.each do |tag|
   Tag.create(name: tag)
 end
+
+challenge_one.challenge_tags.create(tag_id: 1)
