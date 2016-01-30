3dc17e89ae033f37597634e7e562453ae763f552
Hunter Braun <hunter.braun@gmail.com>
2016-01-30 03:26:49 -0600

    allow editing of username

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
+  # Overwrite update_resource to let users to update their user without giving their password
+  def update_resource(resource, params)
+    if current_user.provider == 'github'
+      params.delete('current_password')
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
+  end
 
   private
 
   def sign_up_params
     params.require(:user).permit(:email, :username, :password, :password_confirmation, :remember_me)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation, :current_password)
   end
 end
