64cec3f5bea7c98037fbcdcdffa6e4dcd74abeb9
Hunter Braun <hunter.braun@gmail.com>
2016-01-29 19:28:55 -0600

    initial refactor of all views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def add_active?(test_path)
     return 'active' if request.path == test_path
   end
+
+  def owns_content?(content_name)
+    current_user.id == content_name.user.id
+  end
 end
